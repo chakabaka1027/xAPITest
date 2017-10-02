3fde2f3d54b0b90e954452ff5dc78ae63fd0539b
chakabaka1027 <alexanderdcha@gmail.com>
2017-10-02 17:28:50 -0400

    basic flight mode functioning

--- Assets/Scripts/FlightController.cs
+++ Assets/Scripts/FlightController.cs
@@ -10,39 +10,59 @@ public class FlightController : MonoBehaviour {
     Vector3 walkAmount;
     Vector3 smoothDampMoveRef;
     CharacterController controller;
-    float speed = 4;
+    float speed = 13;
     public float velocityY;
 
     [Header("Look Controls")]
 	[Range (-10, 10)]
-	public float mouseSensitivityX = 6f;
+	public float mouseSensitivityX = 3f;
 	[Range (-10, 10)]
-	public float mouseSensitivityY = 6f;
+	public float mouseSensitivityY = 3f;
 	float verticalLookRotation;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
 	// Use this for initialization
 	void Start () {
-		playerController = GetComponent<PlayerController>();
-        controller = GetComponent<CharacterController>();
+		playerController = FindObjectOfType<PlayerController>();
+        controller = FindObjectOfType<CharacterController>();
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
-	    verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
-		verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
-		Camera.main.transform.localEulerAngles = Vector3.left * verticalLookRotation;
 
-        Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+        if(Input.GetKeyDown(KeyCode.T)){
+            playerController.flyMode = true; 
+        }
 
-        Vector3 newRight = Vector3.Cross(Vector3.up, gameObject.transform.forward);
-        Vector3 newForward = Vector3.Cross(newRight, Vector3.up);	
+        if(playerController.flyMode){
 
-        Vector3 trueMoveDir = (newRight * Input.GetAxisRaw("Horizontal") + newForward * Input.GetAxisRaw("Vertical"));
-        targetWalkAmount = trueMoveDir * speed + Vector3.up * velocityY;
-		walkAmount = Vector3.SmoothDamp(walkAmount, targetWalkAmount, ref smoothDampMoveRef, 0.1f);
-        controller.Move(walkAmount * Time.fixedDeltaTime);
+            yaw += mouseSensitivityX * Input.GetAxis("Mouse X");
+            pitch -= mouseSensitivityY * Input.GetAxis("Mouse Y");
+            pitch = Mathf.Clamp(pitch, -90f, 90f);
 
+            transform.localEulerAngles = new Vector3(pitch, yaw, 0.0f);
+
+            Vector3 moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
+
+            Vector3 newRight = Vector3.Cross(Vector3.up, transform.worldToLocalMatrix.MultiplyVector(transform.forward));
+            Vector3 newForward = Vector3.Cross(newRight, Vector3.up);	
+
+            Vector3 trueMoveDir = (newRight * Input.GetAxisRaw("Horizontal") + newForward * Input.GetAxisRaw("Vertical"));
+            targetWalkAmount = trueMoveDir * speed + Vector3.up * velocityY;
+            walkAmount = Vector3.SmoothDamp(walkAmount, targetWalkAmount, ref smoothDampMoveRef, 0.1f);
+            transform.Translate(walkAmount * Time.fixedDeltaTime);
+
+
+            if(Input.GetKey(KeyCode.E)){
+                transform.Translate(Vector3.up * 13 * Time.fixedDeltaTime);
+            }
+
+            if(Input.GetKey(KeyCode.Q)){
+                transform.Translate(-Vector3.up * 13 * Time.fixedDeltaTime);
+            }
+        }
     }
 }
